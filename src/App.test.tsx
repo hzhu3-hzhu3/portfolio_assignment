10ca72ca809daa85b7dd48fdf2faf14ac59f44f4
hzhu3-hzhu3 <hzhu3@academic.rrc.ca>
2025-04-07 12:26:14 -0500

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('react-router-dom', () => ({
+  BrowserRouter: ({ children }: any) => <div>{children}</div>,
+  Routes: ({ children }: any) => <div>{children}</div>,
+  Route: () => <div>Route</div>,
+  NavLink: ({ children }: any) => <span>{children}</span>,
+}));
+
+test('renders App without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const navElement = screen.getByText(/About Me/i);
+  expect(navElement).toBeInTheDocument();
 });
